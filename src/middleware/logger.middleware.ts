d23de22065fd3d5da91e1ccc87d614d9b2125ab1
kapil <kplsh106@gmail.com>
2024-09-14 20:12:29 +0530

    storing more info on logs

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -1,19 +1,32 @@
 import { Injectable, NestMiddleware } from "@nestjs/common";
-import { NextFunction ,Request} from "express";
+import { NextFunction, Request } from "express";
 import * as geoip from 'geoip-lite'
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-    use(req:Request,res: Response,next:NextFunction) {
-         const ipAddress =
-      req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    use(req: Request, res: Response, next: NextFunction) {
+        const ipAddress =
+            req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        const geo = geoip.lookup(ipAddress);
+        if (geo) {
+            const country = geo.country || 'Unknown';
+            const region = geo.region || 'Unknown';
+            const city = geo.city || 'Unknown';
+            const lat = geo.ll ? geo.ll[0] : 'Unknown';
+            const lon = geo.ll ? geo.ll[1] : 'Unknown';
+            const timezone = geo.timezone || 'Unknown';
 
-    const geo = geoip.lookup(ipAddress as string);
+            console.log(`IP Address: ${ipAddress}`);
+            console.log(`Country: ${country}`);
+            console.log(`Region: ${region}`);
+            console.log(`City: ${city}`);
+            console.log(`Latitude: ${lat}`);
+            console.log(`Longitude: ${lon}`);
+            console.log(`Timezone: ${timezone}`);
+        } else {
+            console.log(`Unable to locate details for IP: ${ipAddress}`);
+        }
 
-    const country = geo ? geo.country : 'Unknown';
-
-    console.log(`IP Address: ${ipAddress}`);
-    console.log(`Country: ${country}`);
 
         next();
     }
